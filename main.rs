a3308f6c4989b64185dbe8e944dc408af2503367
Richard Diamond <wichard@vitalitystudios.com>
2014-07-13 02:16:23 -0500

    Update main.rs for rustc changes.

--- main.rs
+++ main.rs
@@ -5,14 +5,14 @@
 extern crate ppapi;
 extern crate collections;
 
-use collections::hashmap::HashMap;
+use std::collections::hashmap::HashMap;
 
 #[no_mangle]
 #[cfg(target_os = "nacl")]
 // Called when an instance is created. Return a boxed trait for your callbacks.
 pub extern fn ppapi_instance_created
     (instance: ppapi::Instance,
-     _args: || -> HashMap<~str, ~str>) -> Box<ppapi::InstanceCallback> {
+     _args: || -> HashMap<String, String>) -> Box<ppapi::InstanceCallback> {
         use ppapi::ppb::ConsoleInterface;
         let console = instance.console();
         console.log(ppapi::ffi::PP_LOGLEVEL_LOG, "Hello, world!");
