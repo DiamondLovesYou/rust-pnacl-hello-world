30e266101ae932fa3fd8c2cd23e17794430b2da4
Richard Diamond <wichard@vitalitystudios.com>
2014-07-27 14:26:30 -0500

    Tidy up main.rs.

--- main.rs
+++ main.rs
@@ -3,21 +3,19 @@
 #![no_main]
 
 extern crate ppapi;
-extern crate collections;
 
 use std::collections::hashmap::HashMap;
 
 #[no_mangle]
 #[cfg(target_os = "nacl")]
 // Called when an instance is created. Return a boxed trait for your callbacks.
-pub extern fn ppapi_instance_created
-    (instance: ppapi::Instance,
-     _args: || -> HashMap<String, String>) -> Box<ppapi::InstanceCallbacks> {
-        use ppapi::ppb::ConsoleInterface;
-        let console = instance.console();
-        console.log(ppapi::ffi::PP_LOGLEVEL_LOG, "Hello, world!");
-        box NoOpt as Box<ppapi::InstanceCallbacks>
-    }
+pub extern fn ppapi_instance_created(instance: ppapi::Instance,
+                                     _args: || -> HashMap<String, String>) -> Box<ppapi::InstanceCallbacks> {
+    use ppapi::ppb::ConsoleInterface;
+    let console = instance.console();
+    console.log(ppapi::ffi::PP_LOGLEVEL_LOG, "Hello, world!");
+    box NoOpt as Box<ppapi::InstanceCallbacks>
+}
 
 struct NoOpt;
 impl ppapi::InstanceCallbacks for NoOpt { }
