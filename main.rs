8ed6ed9f481e912c13d8708533e9ed3b1391e021
Richard Diamond <wichard@vitalitystudios.com>
2014-07-27 17:27:23 -0500

    Use println!().

--- main.rs
+++ main.rs
@@ -9,11 +9,9 @@ use std::collections::hashmap::HashMap;
 #[no_mangle]
 #[cfg(target_os = "nacl")]
 // Called when an instance is created. Return a boxed trait for your callbacks.
-pub extern fn ppapi_instance_created(instance: ppapi::Instance,
+pub extern fn ppapi_instance_created(_instance: ppapi::Instance,
                                      _args: || -> HashMap<String, String>) -> Box<ppapi::InstanceCallbacks> {
-    use ppapi::ppb::ConsoleInterface;
-    let console = instance.console();
-    console.log(ppapi::ffi::PP_LOGLEVEL_LOG, "Hello, world!");
+    println!("Hello, world!");
     box NoOpt as Box<ppapi::InstanceCallbacks>
 }
 
